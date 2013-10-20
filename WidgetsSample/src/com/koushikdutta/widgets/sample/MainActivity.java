6d1f345e5158c6999e749edbe84ff3a030bc8595
ROM-ANDROID <codekill13@gmail.com>
2013-10-20 18:08:35 -0400

    koush

--- WidgetsSample/src/com/koushikdutta/widgets/sample/MainActivity.java
+++ WidgetsSample/src/com/koushikdutta/widgets/sample/MainActivity.java
@@ -1,20 +1,26 @@
-### To do this:
+package com.koushikdutta.widgets.sample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.koushikdutta.widgets.BetterListActivity;
+import com.koushikdutta.widgets.ListItem;
 
 
-![](https://raw.github.com/koush/Widgets/master/dark.png)
-![](https://raw.github.com/koush/Widgets/master/light.png)
-
-
-### Write this:
-
-
-```java
-
-public class MainActivity extends ActivityBase {
+public class MainActivity extends BetterListActivity {
     @Override
     public void onCreate(Bundle savedInstanceState, View view) {
         super.onCreate(savedInstanceState, view);
         
+        addItem("Content", new ListItem(getFragment(), "List Content", null, R.drawable.ic_launcher) {
+            @Override
+            public void onClick(View view) {
+                super.onClick(view);
+                startActivity(new Intent(MainActivity.this, ListContentTest.class));
+            }
+        });        
         addItem("Theme", new ListItem(getFragment(), "Dark Theme", null, R.drawable.ic_launcher) {
             @Override
             public void onClick(View view) {
@@ -23,6 +29,13 @@ public class MainActivity extends ActivityBase {
             }
         });
         
+        addItem("Theme", new ListItem(getFragment(), "Content", "Dark Theme", R.drawable.ic_launcher) {
+            @Override
+            public void onClick(View view) {
+                super.onClick(view);
+                startActivity(new Intent(MainActivity.this, ListContentTestDark.class));
+            }
+        });
         addItem(R.string.cloud, new ListItem(getFragment(), R.string.googledrive, R.string.googledrive_summary, R.drawable.drive) {
             @Override
             public void onClick(View view) {
@@ -40,6 +53,3 @@ public class MainActivity extends ActivityBase {
         addItem(R.string.devices, new ListItem(getFragment(), "Nexus 10", null, R.drawable.nexusone)).setCheckboxVisible(true);
     }
 }
-
-
-```
